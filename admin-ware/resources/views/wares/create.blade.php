4070c664c9e226dcf0ba67496d4599a45f9a3d85
alyviandi <alyzaraviandi@students.unnes.ac.id>
2023-03-28 00:35:18 +0700

    crud alyzaar

---
+++ admin-ware/resources/views/wares/create.blade.php
@@ -0,0 +1,54 @@
+@extends('wares.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New WareKeepers</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('wares.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<form action="{{ route('wares.store') }}" method="POST">
+    @csrf
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>WareKeeper Id:</strong>
+                <input type="text" name="wareid" class="form-control" placeholder="WareKeeper Id">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="namaware" class="form-control" placeholder="Nama WareKeeper">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Jabatan:</strong>
+                <input type="text" name="jabatanware" class="form-control" placeholder="Jabatan WareKeeper">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
