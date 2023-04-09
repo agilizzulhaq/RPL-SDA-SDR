9efd4f1834935938c6bf7b911b78ad14f41acb50
arjunamhv <mhv@students.unnes.ac.id>
2023-04-09 21:10:13 +0700

    arjuna2

---
+++ app/Http/Requests/UpdateRuangansRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRuangansRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'txtnama' => 'required',
+            'txtlokasi' => 'required',
+            'txtkondisi' => 'required',
+            'txtstatus' => 'required',
+            'txtstatusp' => 'required',
+            'txthistory' => 'required',
+            'txtjenis' => 'required',
+            'txtkapasitas' => 'required',
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'txtnama.required' => 'Oops, :attribute  tidak boleh kosong',
+            'txtlokasi.required' => 'Oops, :attribute  tidak boleh kosong',
+            'txtkondisi.required' => 'Pilih salah satu',
+            'txthistory.required' => 'Oops, :attribute tidak boleh kosong',
+            'txtjenis.required' => 'Oops, :attribute tidak boleh kosong',
+            'txtstatus.required' => 'Oops, :attribute tidak boleh kosong',
+            'txtstatusp.required' => 'Oops, :attribute tidak boleh kosong',
+            'txtkapasitas.required' => 'Oops, :attribute tidak boleh kosong',
+        ];
+    }
+    public function attributes(): array
+    {
+        return [
+            'txtnama' => 'Nama Alat',
+            'txtlokasi' => 'lokasi',
+            'txtkondisi' => 'Kondisi Alat',
+            'txtstatus' => 'Status',
+            'txtstatusp' => 'Status Perawatan',
+            'txthistory' => 'Tanggal Terakhir Dirawat',
+            'txtjenis' => 'Jenis Ruangan',
+            'txtkapasitas' => 'Kapasitas Ruangan',
+        ];
+    }
+}
