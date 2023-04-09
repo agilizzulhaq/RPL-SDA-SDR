42f3b78083f3af297d7b8165beebd08df65b4f32
Rasikh Khalil Pasha <raxiest@gmail.com>
2023-04-09 13:46:28 +0700

    menambah folder laravel

---
+++ kel1-rpl/resources/views/rpl.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RPL</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="css/style_rpl.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+</head>
+<body>
+    <nav class="bg-[#D9D9D9] w-full h-[105px] flex">
+        <div class="bg-[#ACACAC] w-[324px] h-full"></div>
+        <h1 class="font-bold text-3xl ml-24 my-auto ">Manajemen Sumber Daya Alat Dan Kesehatan</h1>
+    </nav>
+    <div class="flex">
+        <nav class="bg-[#D9D9D9] w-[324px] h-[calc(100vh-105px)]">
+            <div class="flex w-full items-center">
+                <div class="mt-[21px] ml-[17px] w-[105px] h-[105px] bg-[#ACACAC] rounded-full flex justify-center items-center">
+                    <span class="material-icons" style="font-size: 80px;">person</span>
+                </div>
+                <div class="ml-5 mt-[21px]">
+                    <p class="text-lg font-bold mb-3">Nama</p>
+                    <div class="w-[14px] h-[14px] bg-[#00FF47] rounded-full inline-block"></div>
+                    <span class="font-bold text-sm ml-2">Online</span>
+                </div>
+            </div>
+            <div class="mt-10">
+                <div class="flex items-center pl-[17px] py-3 hover:bg-[#FFFFFF] w-full">
+                    <span class="material-icons" style="font-size: 40px;">home</span>
+                    <p class="text-xl font-bold ml-5">Dashboard</p>
+                </div>
+                <div class="flex items-center pl-[17px] py-3 hover:bg-[#FFFFFF]">
+                    <span class="material-icons" style="font-size: 40px;">vaccines</span>
+                    <p class="text-xl font-bold ml-5">Sumber Daya Alat</p>
+                </div>
+                <div class="flex items-center pl-10 py-3 hover:bg-[#FFFFFF]">
+                    <span class="material-icons" style="font-size: 25px;">chevron_right</span>
+                    <p class="text-lg font-bold ml-3">Peminjaman Pengembalian</p>
+                </div>
+                <div class="flex items-center pl-10 py-3 hover:bg-[#FFFFFF]">
+                    <span class="material-icons" style="font-size: 25px;">chevron_right</span>
+                    <p class="text-lg font-bold ml-3">Perawatan Pemeliharaan</p>
+                </div>
+                <div class="flex items-center pl-10 py-3 hover:bg-[#FFFFFF]">
+                    <span class="material-icons" style="font-size: 25px;">chevron_right</span>
+                    <p class="text-lg font-bold ml-3">Pengadaan Pembelian</p>
+                </div>
+
+                <div class="flex items-center pl-[17px] py-3 hover:bg-[#FFFFFF]">
+                    <span class="material-icons" style="font-size: 40px;">apartment</span>
+                    <p class="text-xl font-bold ml-5">Sumber Daya Ruangan</p>
+                </div>
+            </div>
+        </nav>
+        <main>
+            <p class="mt-96 ml-96">kodenya disini</p>
+        </main>
+    </div>
+</body>
+</html>
