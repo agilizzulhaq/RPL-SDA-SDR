4577dbc3a0c1cab1edf7c39debeb29d696dcc695
agilizzulhaq <agilizzulhaq@students.unnes.ac.id>
2023-04-11 09:26:24 +0700

    Benerin controller

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -2,47 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Room;
+use App\Models\Ruangan;
 use Illuminate\Http\Request;
 
-class RoomController extends Controller
+class RuanganController extends Controller
 {
-    # Controller Data Room
+    # Controller Data Ruangan
     public function ruangan(Request $request) {
         if($request -> has ('search')) {
-            $data = Room::where('namaRuangan','LIKE','%' .$request -> search.'%') -> paginate(5);
+            $data = Ruangan::where('namaRuangan','LIKE','%' .$request -> search.'%') -> paginate(5);
         } else {
-            $data = Room::paginate(5);
+            $data = Ruangan::paginate(5);
         }
-        return view('room.dataruangan', compact('data'));
+        return view('ruangan.dataruangan', compact('data'));
     }
 
     # Controller Input Data
     public function tambahruangan() {
-        return view('room.tambahdata');
+        return view('ruangan.tambahdata');
     }
 
-    public function masukkandata(Request $request) {
+    public function masukkanruangan(Request $request) {
         //dd($request->all());
-        Room::create($request->all());
+        Ruangan::create($request->all());
         return redirect()->route('ruangan') -> with('success','Data berhasil ditambahkan');
     }
 
     # Controller Edit Data
-    public function editdata($id) {
-        $data = Room::find($id);
+    public function editruangan($id) {
+        $data = Ruangan::find($id);
         //dd($data);
-        return view('room.editdata', compact('data'));
+        return view('ruangan.editdata', compact('data'));
     }
     
-    public function updatedata(Request $request, $id) {
-        $data = Room::find($id);
+    public function updateruangan(Request $request, $id) {
+        $data = Ruangan::find($id);
         $data -> update($request -> all());
         return redirect()->route('ruangan') -> with('success','Data berhasil diperbaharui');
     }
 
-    public function hapusdata($id) {
-        $data = Room::find($id);
+    public function hapusruangan($id) {
+        $data = Ruangan::find($id);
         $data -> delete();
         return redirect()->route('ruangan') -> with('success','Data berhasil dihapus');
     }
