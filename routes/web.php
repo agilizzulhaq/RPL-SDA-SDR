4d7cec109aa4b4ce2712924423f6459d1b284ff9
alyviandi <alyzaraviandi@students.unnes.ac.id>
2023-04-17 15:56:03 +0700

    logib2

--- routes/web.php
+++ routes/web.php
@@ -89,8 +89,6 @@ Route::resource('admins', AdminController::class);
 Route::resource('wares', WareController::class);
 
 Route::controller(LoginRegisterController::class)->group(function () {
-    Route::get('/register', 'register')->name('register');
-    Route::post('/store', 'store')->name('store');
     Route::get('/login', 'login')->name('login');
     Route::post('/authenticate', 'authenticate')->name('authenticate');
     Route::get('/dashboard', function () {
