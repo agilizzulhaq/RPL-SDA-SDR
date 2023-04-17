b5b9c76901a52ac154a76ce9a1d37510d73d0ecb
alyviandi <alyzaraviandi@students.unnes.ac.id>
2023-04-17 16:25:38 +0700

    login4

--- routes/web.php
+++ routes/web.php
@@ -92,7 +92,7 @@ Route::controller(LoginRegisterController::class)->group(function () {
     Route::get('/login', 'login')->name('login');
     Route::post('/authenticate', 'authenticate')->name('authenticate');
     Route::get('/dashboard', function () {
-        return view('dashboard-admin');
+        return view('dashboard');
     })->name('dashboard');
     Route::post('/logout', 'logout')->name('logout');
 });
