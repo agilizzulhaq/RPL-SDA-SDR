8005e5e238bee86a14f884b7991344012f22d13d
alyviandi <alyzaraviandi@students.unnes.ac.id>
2023-05-13 12:58:58 +0700

    perbaiki typo2

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\WareController;
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Auth\AdminController;
 use App\Http\Controllers\LokasiController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\ProductController;
