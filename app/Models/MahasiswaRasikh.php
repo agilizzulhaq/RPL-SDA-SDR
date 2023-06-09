3c5d2ae503eb1a71e18f1d5e6c7722c830181565
Rasikh Khalil Pasha <raxiest@gmail.com>
2023-06-09 14:36:41 +0700

    fix crud rasikh

--- app/Models/MahasiswaRasikh.php
+++ app/Models/MahasiswaRasikh.php
@@ -7,12 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class MahasiswaRasikh extends Model
 {
-    protected $table = '_Mahasiswa_rasikh_khalil_pasha_060623';
+    protected $table = '_mahasiswa_rasikh_khalil_pasha_060623';
     protected $guarded = [];
     
     use HasFactory;
 
-    public function nilaimks(){
-        return $this->belongsTo(MataKuliahRasikh::class);
-    }
 }
