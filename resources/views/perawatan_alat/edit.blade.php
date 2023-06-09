558714055ac414ff346db605324c3e5dcb7638df
ReginaAyumi <reginaayumi@students.unnes.ac.id>
2023-06-09 20:21:53 +0700

    fix edit perawatan alat

--- resources/views/perawatan_alat/edit.blade.php
+++ resources/views/perawatan_alat/edit.blade.php
@@ -1,9 +1,10 @@
 @extends('../layout2/mainnew')
 
 @section('isi')
-    <div class="w-[1040px]">
+    <div class="mt-16 mx-10">
+        <h1 class="text-3xl text-black font-bold mb-5">Edit Perawatan Alat</h1>
 
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
                     <h2>Edit Data Perawatan</h2>
@@ -23,23 +24,20 @@
                     @endforeach
                 </ul>
             </div>
-        @endif
-      
-        <form action="{{ route('perawatan_alat.update',$perawatan_alat->id_perawatan)  }}" method="POST">
-            @csrf
-            @method('PUT')
-       
-             <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>ID Perawatan:</strong>
-                        <input type="text" name="id_perawatan" value="{{ $perawatan_alat->id_perawatan }}" class="form-control" placeholder="Kode Alat">
+        @endif --}}
+        <div class="w-full bg-white rounded-xl text-sm text-black-md p-3 border-2 border-green-200">
+            <form action="{{ route('perawatan_alat.update',$perawatan_alat->id_perawatan)  }}" method="POST">
+                @csrf
+                @method('PUT')
+        
+                <div class="">
+                    <div class="mb-3 flex justify-between">
+                        <strong class="form-label text-black">ID Perawatan:</strong>
+                        <input type="text" name="id_perawatan" value="{{ $perawatan_alat->id_perawatan }}" class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" placeholder="Kode Alat">
                     </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Kode Alat:</strong>
-                        <select class="form-select" name="kode_alat" class="form-control" id="kode_alat" value="{{ $perawatan_alat->kode_alat }}" aria-label="Default select example"> 
+                    <div class="mb-3 flex justify-between">
+                        <strong class="form-label text-black">Kode Alat:</strong>
+                        <select class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" name="kode_alat" class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" id="kode_alat" value="{{ $perawatan_alat->kode_alat }}" aria-label="Default select example"> 
                             @foreach ($inventory as $item)
                                 <option value="{{ $item->kodeAlat }}" @if ($item->kodeAlat == $perawatan_alat->kode_alat) selected @endif>
                                     {{ $item->kodeAlat . ' | ' . $item->nama_alat->nama_alat }}
@@ -47,22 +45,18 @@
                             @endforeach
                         </select>
                     </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Jenis Perawatan:</strong>
-                        <select class="form-select" name="jenis_perawatan" aria-label="Default select example">
+                    <div class="mb-3 flex justify-between">
+                        <strong class="form-label text-black">Jenis Perawatan:</strong>
+                        <select class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" name="jenis_perawatan" aria-label="Default select example">
                             <option selected>{{ $perawatan_alat -> jenis_perawatan }}</option>
                             <option value="Perawatan rutin">Perawatan rutin</option>
                             <option value="Pembersihan alat">Pembersihan alat</option>
                             <option value="Perawatan kerusakan">Perawatan kerusakan</option>
                         </select>
                     </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Status Perawatan:</strong>
-                        <select class="form-select" name="status_perawatan" aria-label="Default select example">
+                    <div class="mb-3 flex justify-between">
+                        <strong class="form-label text-black">Status Perawatan:</strong>
+                        <select class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" name="status_perawatan" aria-label="Default select example">
                             <option selected>{{ $perawatan_alat -> status_perawatan }}</option>
                             <option value="Belum konfirmasi">Belum konfirmasi</option>
                             <option value="Konfirmasi untuk perawatan">Konfirmasi untuk perawatan</option>
@@ -70,30 +64,25 @@
                             <option value="Perawatan selesai">Perawatan selesai</option>
                         </select>
                     </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Tanggal Perawatan:</strong>
-                        <input type="datetime-local" value="{{ $perawatan_alat->tanggal_perawatan }}" class="form-control" name="tanggal_perawatan" placeholder="Tanggal Perawatan">
+                    <div class="mb-3 flex justify-between">
+                        <strong class="form-label text-black">Tanggal Perawatan:</strong>
+                        <input type="datetime-local" value="{{ $perawatan_alat->tanggal_perawatan }}" class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" name="tanggal_perawatan" placeholder="Tanggal Perawatan">
+                    </div>
+                    <div class="mb-3 flex justify-between">
+                        <strong class="form-label text-black">Riwayat Perawatan:</strong>
+                        <input type="text" value="{{ $perawatan_alat->riwayat_perawatan }}" class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" name="riwayat_perawatan" placeholder="Riwayat Perawatan">
+                    </div>
+                    <div class="mb-3 flex justify-between">
+                        <strong class="form-label text-black">Catatan Perawatan:</strong>
+                        <input type="text" value="{{ $perawatan_alat->catatan_perawatan }}" class="rounded text-sm py-1 px-2 text-black w-[700px]  border-1 border-black" name="catatan_perawatan" placeholder="Catatan Perawatan">
+                    </div>
+                    <div class="w-full flex justify-end">
+                        <a class="px-4 py-2 bg-[#af0e0e] hover:bg-[#c11f1f] no-underline rounded text-white" href="{{ route('perawatan_alat.index') }}">Cancel</a>
+                        <button type="submit" class="ml-5 px-4 py-2 bg-blue-600 hover:bg-blue-500 rounded text-white">Submit</button>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Riwayat Perawatan:</strong>
-                        <input type="text" value="{{ $perawatan_alat->riwayat_perawatan }}" class="form-control" name="riwayat_perawatan" placeholder="Riwayat Perawatan">
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Catatan Perawatan:</strong>
-                        <input type="text" value="{{ $perawatan_alat->catatan_perawatan }}" class="form-control" name="catatan_perawatan" placeholder="Catatan Perawatan">
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </div>
-       
-        </form>
+        
+            </form>
+        </div>
     </div>
 @endsection
